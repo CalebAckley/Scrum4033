3b6698937466c1685bf79efc2f2547e18003352b
kannshort <130014405+kannshort@users.noreply.github.com>
2023-04-05 20:36:46 -0500

    Add files via upload

---
+++ createprojectDB.sql
@@ -0,0 +1,24 @@
+﻿DROP DATABASE IF EXISTS acmemedical;
+CREATE DATABASE IF NOT EXISTS acmemedical;
+USE acmemedical;
+-- create a new table
+CREATE TABLE patients (
+   patId INT AUTO_INCREMENT NOT NULL,
+   patFirst VARCHAR(100) NOT NULL,
+   patLast VARCHAR(150) NOT NULL,
+   patGender VARCHAR(1) NOT NULL,
+   patBithdate DATETIME NOT NULL,
+   patGenetics VARCHAR(250) NOT NULL,
+   patDiabetes VARCHAR(3) NOT NULL
+   patOther VARCHAR(150),
+   PRIMARY KEY (patID)
+);
+
+
+
+-- CREATE USER 
+CREATE USER 'kermit'@'localhost' IDENTIFIED BY 'sesame';
+
+USE acmemedical;
+GRANT SELECT, INSERT, UPDATE ON patients TO 'kermit'@'localhost';
+FLUSH PRIVILEGES;
