54f6c983405d86148475af55e4ee99f029aba457
kannshort <130014405+kannshort@users.noreply.github.com>
2023-04-10 22:28:27 -0500

    Update createprojectDB.sql

--- createprojectDB.sql
+++ createprojectDB.sql
@@ -1,21 +1,21 @@
-﻿DROP DATABASE IF EXISTS acmemedical;
+DROP DATABASE IF EXISTS acmemedical;
 CREATE DATABASE IF NOT EXISTS acmemedical;
 USE acmemedical;
 -- create a new table
-CREATE TABLE patients (
-   patId INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS patients (
+   patId INT NOT NULL AUTO_INCREMENT,
    patFirst VARCHAR(100) NOT NULL,
    patLast VARCHAR(150) NOT NULL,
-   patGender ENUM('Male', 'Female', 'Prefer not to disclose') NOT NULL,
+   patGender ENUM('Male', 'Female', 'Prefer not to disclose'),
    patBithdate DATE NOT NULL,
    patGenetics VARCHAR(250) NOT NULL,
-   patDiabetes ENUM('Yes', 'No') NOT NULL
+   patDiabetes ENUM('Yes', 'No') NOT NULL,
    patOther VARCHAR(150),
    PRIMARY KEY (patID)
 );
 
-CREATE TABLE medications (
-   medID INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS medications (
+   medID INT NOT NULL AUTO_INCREMENT,
    patId INT NOT NULL,
    medVEST ENUM('Y', 'N') NOT NULL,
    medAcap ENUM('Y', 'N') NOT NULL,
@@ -31,22 +31,22 @@ CREATE TABLE medications (
    FOREIGN KEY (patId) REFERENCES patients(patId)
 );
 
-CREATE TABLE visits (
+CREATE TABLE IF NOT EXISTS visits (
   patId INT,
-  visitDate DATE NOT NULL,
+  visitDate DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   visitDoc VARCHAR(75) NOT NULL,
   PRIMARY KEY (patId, visitDate),
   FOREIGN KEY (patId) REFERENCES patients(patId) 
 );
 
-CREATE TABLE fev1 )
+CREATE TABLE IF NOT EXISTS fev1 (
    patId INT,
-   testDate DATE NOT NULL,
+   testDate DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
    firstTest INT(3) NOT NULL,
    secondTest INT(3),
    thirdTest INT(3),
    PRIMARY KEY (patId, testDate),
-   FOREIGN KEY (patID) REFERENCES patients(patId)
+   FOREIGN KEY (patId) REFERENCES patients(patId)
 );
 
 -- CREATE USER 
