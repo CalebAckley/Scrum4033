d95b6ba5f0a1d16f581736b739b35c4e099f4487
kannshort <130014405+kannshort@users.noreply.github.com>
2023-04-11 10:16:00 -0500

    Update fev_create.php

--- fev_create.php
+++ fev_create.php
@@ -5,16 +5,16 @@ $pdo = pdo_connect_mysql();
 $msg = '';
 // Check if POST data is not empty
 if (!empty($_POST)) {
-
-    $patId = isset($_POST['patId']) && !empty($_POST['patId']) && $_POST['patId'] != 'auto' ? $_POST['orderID'] : NULL;
+    $entryId = isset($_POST['entryId']) && !empty($_POST['entryId']) && $_POST['entryId'] != 'auto' ? $_POST['entryId'] : NULL;
+    $patId = isset($_POST['patId']) && !empty($_POST['patId']) && $_POST['patId'] != 'auto' ? $_POST['patId'] : NULL;
     $testDate = isset($_POST['testDate']) ? $_POST['testDate'] : '';
     $firstTest = isset($_POST['firstTest']) ? $_POST['firstTest'] : '';
     $secondTest = isset($_POST['secondTest']) ? $_POST['secondTest'] : '';
     $thirdTest = isset($_POST['thirdTest']) ? $_POST['thirdTest'] : '';
    
     // Insert new record into the fev1 table
-    $stmt = $pdo->prepare('INSERT INTO fev1 VALUES (?, ?, ?, ?, ?)');
-    $stmt->execute([$patId, $testDate, $firstTest, $secondTest, $thirdTest]);
+    $stmt = $pdo->prepare('INSERT INTO fev1 VALUES (?, ?, ?, ?, ?, ?)');
+    $stmt->execute([$entryId, $patId, $testDate, $firstTest, $secondTest, $thirdTest]);
     // Output message
     $msg = 'Created Successfully!';
 }
@@ -24,28 +24,31 @@ if (!empty($_POST)) {
 <div class="content update">
 	<h2>Input FEV1 Test Values</h2>
     <form action="fev_create.php" method="post">
-        <label for="patId">Patient ID</label>
-        <label for="testDate">Date of Test</label>
-        <input type="text" name="patId" placeholder="##" value="auto" id="patId">
-        <input type="date" name="testDate" placeholder="Input date" id="testDate">
+        <label for="entryId">Entry ID</label>
+        <label for="patId">Patient</label>
+        <input type="text" name="entryId" placeholder="##" value="auto" id="entryId">
        
         <?php
         $stmt = $pdo->query("SELECT patId, patFirst, patLast FROM patients");
         $patients = $stmt->fetchAll(PDO::FETCH_ASSOC);
       
         ?>
-
-        <label for="patId">Patient</label>
-        <label></label>
-
             <select name="patId" id="patId">
             <?php foreach($patients as $patient) : ?>
             <option value="<?php echo $patient['patId']; ?>"><?php echo $patient['patId'] . ' - ' . $patient['patFirst'] . ' ' . $patient['patLast']; ?></option>
             <?php endforeach; ?>
             </select>
-      
-        <label></label>
-        <label></label>
+        
+        <label for="testDate">Date of Test</label>
+        <label for="firstTest">1st FEV1</label>
+        <input type="date" name="testDate" placeholder="Input date" id="testDate">
+        <input type="text" name="firstTest" placeholder="##" id="firstTest">
+
+        <label for="secondTest">2nd FEV1</label>
+        <label for="thirdTest">3rd FEV1</label>
+        <input type="text" name="secondTest" placeholder="##" id="secondTest">   
+        <input type="text" name="thirdTest" placeholder="##" id="thirdTest">
+
         <input type="submit" value="Create">
     </form>
     <?php if ($msg): ?>
