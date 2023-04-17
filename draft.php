194dc836f505f084a26c1a29d18039c3388f0982
kannshort <130014405+kannshort@users.noreply.github.com>
2023-04-16 19:14:45 -0500

    Update draft.php

--- draft.php
+++ draft.php
@@ -1,50 +1,53 @@
 <?php
     @include_once ('../../app_config.php');
     @include_once (APP_ROOT.APP_FOLDER_NAME . '/scripts/functions.php');
-    // Connect to MySQL database
+
     $pdo = pdo_connect_mysql();
-    // Get the page via GET request (URL param: page), if non exists default the page to 1
-    $page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
-    // Number of records to show on each page
-    $records_per_page = 10;
-    // Prepare the SQL statement and get records from our patients table, LIMIT will determine the page
-    $stmt = $pdo->prepare('SELECT * FROM patients ORDER BY patId LIMIT :current_page, :record_per_page');
-    $stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
-    $stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
-    $stmt->execute();
-    // Fetch the records so we can display them in our template.
-    $patients = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    // Get the total number of patients, this is so we can determine whether there should be a next and previous button
-    $num_patients = $pdo->query('SELECT COUNT(*) FROM patients')->fetchColumn();
-    //$selected_patient = $pdo->prepare('SELECT COUNT(*) FROM patients WHERE patId =');
+    $msg = '';
+    // Check if the patient patId exists, for example //update.php?patId=1 will get the patient with the patId //of 1
+    
+    if (isset($_GET['patId'])) {
+        if (!empty($_POST)) {
+            // This part is similar to the create.php, //but instead we update a record and not //insert
+            $patId = isset($_POST['patId']) ? $_POST['patId'] : NULL;
+            $patFirst = isset($_POST['patFirst']) ? $_POST['patFirst'] : '';
+            $patLast = isset($_POST['patLast']) ? $_POST['patLast'] : '';
+            $patGender = isset($_POST['patGender']) ? $_POST['patGender'] : '';
+            $patBirthdate = isset($_POST['patBirthdate']) ? $_POST['patBirthdate'] : date('Y-m-d');
+            $patGenetics = isset($_POST['patGenetics']) ? $_POST['patGenetics'] : '';
+            $patDiabetes = isset($_POST['patDiabetes']) ? $_POST['patDiabetes'] : '';
+            $patOther = isset($_POST['patOther']) ? $_POST['patOther'] : '';
+            // Update the record
+        }
+        // Get the patient from the patients table
+        $stmt = $pdo->prepare('SELECT * FROM patients WHERE patId = ?');
+        $stmt->execute([$_GET['patId']]);
+        $patient = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    }
 
     if (isset($_GET['patId'])) {
         if (!empty($_POST)) {
-            $entryId = isset($_POST['entryId']) ? $_POST['entryId'] : NULL;
-            $patId = isset($_POST['patId']) ? $_POST['patId'] : NULL;
+            // This part is similar to the create.php, //but instead we update a record and not //insert
+            $entryId = isset($_POST['entryId']) ? $_POST['entryId'] : '';
+            $patId = isset($_POST['patId']) ? $_POST['patId'] : '';
             $testDate = isset($_POST['testDate']) ? $_POST['testDate'] : '';
             $firstTest = isset($_POST['firstTest']) ? $_POST['firstTest'] : '';
             $secondTest = isset($_POST['secondTest']) ? $_POST['secondTest'] : '';
-            $thirdTest = isset($_POST['thirdTest']) ? $_POST['thirdTest'] : '';  
-        }        
-    
-        $fev_stmt = $pdo->prepare('SELECT * from fev1 WHERE patId = ?');
-        $fev_stmt->execute([$_GET['patId']]);
-        $entries = $fev_stmt->fetch(PDO::FETCH_ASSOC);
+            $thirdTest = isset($_POST['thirdTest']) ? $_POST['thirdTest'] : '';
 
-        if (!$entries) {
-            exit('Test doesn\'t exist with that Patient ID!');
-        }
-        else {
-            exit('No ID specified!');
+            $fevstmt = $pdo->prepare('SELECT * FROM fev1 WHERE patId = ?');
+            $fevstmt->execute([$_GET['patId']]);
+            $entries = $fevstmt->fetch(PDO::FETCH_ASSOC);
+
         }
     }
 
 ?>
-
 <?=template_header('Read')?>
+
     <div class="content read">
-        <h2>Patient File</h2>
+        <h2>Patient #<?=$patient['patId']?></h2>
         <table>
             <thead>
                 <tr>
@@ -60,7 +63,6 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($patients as $patient): ?>
                 <tr>
                     <td><?=$patient['patId']?></td>
                     <td><?=$patient['patFirst']?></td>
@@ -71,55 +73,43 @@
                     <td><?=$patient['patDiabetes']?></td>
                     <td><?=$patient['patOther']?></td>
                     <td class="actions">
-                        <a href="draft.php?patId=<?=$patient['patId']?>" class="select"><i class="fas fa-hand-pointer" style="color: #ffffff;"></i></a>
                         <a href="patients_update.php?patId=<?=$patient['patId']?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
                         <a href="patients_delete.php?patId=<?=$patient['patId']?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
                     </td>
                 </tr>
-                <?php endforeach; ?>
             </tbody>
         </table><br>
 
-    <h2>FEV1 Test</h2>        
-	<a href="fev_create.php" class="create-contact">Input New Tests</a>
-    <table>
-        <thead>
-            <tr>
-                <td>Entry ID</td>
-                <td>Patient ID</td>
-                <td>Test Date</td>
-                <td>FEV 1</td>
-                <td>FEV 1</td>
-                <td>FEV 1</td>
-                <td></td>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($entries as $entry): ?>            
-            <tr>
-                <td><?=$entry['entryId']?></td>
-                <td><?=$entry['patId']?></td>
-                <td><?=$entry['testDate']?></td>
-                <td><?=$entry['firstTest']?></td>
-                <td><?=$entry['secondTest']?></td>
-                <td><?=$entry['thirdTest']?></td>
-                <td class="actions">
-                    <a href="fev_update.php?entryId=<?=$entry['entryId']?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
-                    <a href="fev_delete.php?entryId=<?=$entry['entryId']?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+        <h2>FEV1 Tests</h2>
+	    <a href="fev_create.php" class="create-contact">Input New Tests</a>
+	    <table>
+            <thead>
+                <tr>
+                    <td>Entry ID</td>
+                    <td>Patient ID</td>
+                    <td>Test Date</td>
+                    <td>FEV 1</td>
+                    <td>FEV 1</td>
+                    <td>FEV 1</td>
+                    <td></td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><?=$entries['entryId']?></td>
+                    <td><?=$entries['patId']?></td>
+                    <td><?=$entries['testDate']?></td>
+                    <td><?=$entries['firstTest']?></td>
+                    <td><?=$entries['secondTest']?></td>
+                    <td><?=$entries['thirdTest']?></td>
+                    <td class="actions">
+                        <a href="fev_update.php?entryId=<?=$entries['entryId']?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
+                        <a href="fev_delete.php?entryId=<?=$entries['entryId']?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
 
-        <div class="pagination">
-            <?php if ($page > 1): ?>
-            <a href="patients_read.php?page=<?=$page-1?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
-            <?php endif; ?>
-            <?php if ($page*$records_per_page < $num_patients): ?>
-            <a href="patients_read.php?page=<?=$page+1?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
-            <?php endif; ?>
-        </div>
     </div>
 
 <?=template_footer()?>
