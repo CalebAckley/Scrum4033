e71cd584033703408dd19301c117e1db7010308b
Sam <89803535+Samuknight@users.noreply.github.com>
2023-04-17 15:09:47 -0500

    Add files via upload

--- medication_update.php
+++ medication_update.php
@@ -1,13 +1,13 @@
 <?php
-@include_once ('../../app_config.php');
-@include_once (APP_ROOT.APP_FOLDER_NAME . '/scripts/functions.php');
-$pdo = pdo_connect_mysql();
-$msg = '';
-// Check if the contact id exists, for example //update.php?id=1 will get the contact with the id //of 1
-if (isset($_GET['medID'])) {
-    if (!empty($_POST)) {
-        // This part is similar to the create.php, //but instead we update a record and not //insert
-        $medID = isset($_POST['medID']) ? $_POST['medID'] : NULL;
+    @include_once ('../../app_config.php');
+    @include_once (APP_ROOT.APP_FOLDER_NAME . '/scripts/functions.php');
+    $pdo = pdo_connect_mysql();
+    $msg = '';
+    // Check if the patient patId exists, for example //update.php?patId=1 will get the patient with the patId //of 1
+    if (isset($_GET['medID'])) {
+        if (!empty($_POST)) {
+            // This part is similar to the create.php, //but instead we update a record and not //insert
+            $medID = isset($_POST['medID']) ? $_POST['medID'] : NULL;
         $patId = isset($_POST['patId']) ? $_POST['patId'] : NULL;
         $medVEST = isset($_POST['medVEST']) ? $_POST['medVEST'] : '';
         $medAcap = isset($_POST['medAcap']) ? $_POST['medAcap'] : '';
@@ -22,31 +22,32 @@ if (isset($_GET['medID'])) {
         $medGent = isset($_POST['medGent']) ? $_POST['medGent'] : '';
         $medEnzy = isset($_POST['medEnzy']) ? $_POST['medEnzy'] : '';
         $enzyDate = isset($_POST['enzyDate']) ? $_POST['enzyDate'] : '';
-        // Update the record
-        $stmt = $pdo->prepare('UPDATE medications SET medID = ?, patId= ?, medVEST = ?, medPlum = ?, plumQuant = ?, plumDate = ?, medTobi = ?, medColi = ?, medHype = ?, medAzit = ?, medClar = ?, medGent = ?, medEnzy = ?, enzyDate = ? WHERE medID = ?');
-        $stmt->execute([$medID, $patId, $medVEST, $medAcap, $medPlum, $plumQuant, $plumDate, $medTobi, $medColi, $medHype, $medAzit, $medClar, $medGent, $medEnzy, $enzyDate, $_GET['medID']]);
+            // Update the record
+            $stmt = $pdo->prepare('UPDATE medications SET medID = ?, patId= ?, medVEST = ?, medAcap = ?, medPlum = ?, plumQuant = ?, plumDate = ?, medTobi = ?, medColi = ?, medHype = ?, medAzit = ?, medClar = ?, medGent = ?, medEnzy = ?, enzyType = ? WHERE medID = ?');
+        $stmt->execute([$medID, $patId, $medVEST, $medAcap, $medPlum, $plumQuant, $plumDate, $medTobi, $medColi, $medHype, $medAzit, $medClar, $medGent, $medEnzy, $enzyType, $_GET['medID']]);
         $msg = 'Updated Successfully!';
+        }
+        // Get the patient from the patients table
+        $stmt = $pdo->prepare('SELECT * FROM medications WHERE medID = ?');
+        $stmt->execute([$_GET['medID']]);
+        $medication = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$medication) {
+            exit('medication form doesn\'t exist with that ID!');
+        }
+    } else {
+        exit('No ID specified!');
     }
-    // Get the contact from the contacts table
-    $stmt = $pdo->prepare('SELECT * FROM medications WHERE medID = ?');
-    $stmt->execute([$_GET['medID']]);
-    $medication = $stmt->fetch(PDO::FETCH_ASSOC);
-    if (!$medication) {
-        exit('Medication form doesn\'t exist with that ID!');
-    }
-} else {
-    exit('No ID specified!');
-}
 ?>
 <?=template_header('Read')?>
 
 <div class="content update">
 	<h2>Update Medication Form #<?=$medication['medID']?></h2>
-    <form action="medication_update.php?id=<?=$medication['medID']?>" method="post">
-    <label for="medID">MedID</label>
+    <form action="medication_update.php?medID=<?=$medication['medID']?>" method="post">
+        <label for="medID">MedID</label>
         <label for="patId">patID</label>
-        <input type="text" name="medID" placeholder="26" value="auto" id="id">
-        <input type="text" name="patiD" placeholder="26" value="auto" id="id">
+        <input type="text" name="medID" placeholder="26" value="auto" id="medID">
+        <input type="text" name="patId" placeholder="##" value="<?=$medication['patId']?>" id="patId">
+        <lable></label>
         <label for="medVEST">Vest</label>
         <label></label>
         <select name="medVEST" id="medVEST"> <option value="N">No</option> <option value="Y">Yes</option> </select>
@@ -58,7 +59,7 @@ if (isset($_GET['medID'])) {
         <select name="medPlum" id="medPlum"> <option value="N">No</option> <option value="Y">Yes</option> </select>
         <label for="plumQuant">Plumozyme Quantity</label>
         <label></label>
-        <input type="text" name="plumeQuant" placeholder="Quantity" id="plumQuant">
+        <input type="text" name="plumQuant" placeholder="Quantity" id="plumQuant">
         <label></label>
         <label for="plumDate">Plumozyme Date</label>
         <label></label>
@@ -85,10 +86,11 @@ if (isset($_GET['medID'])) {
         <label for="medEnzy">Enzymes</label>
         <label></label>
         <select name="medEnzy" id="medEnzy"> <option value="N">No</option> <option value="Y">Yes</option> </select>
-        <label for="enzyDate">Enzymes</label>
+        <label for="enzyDate">Enzyme Type/Dosage</label>
         <label></label>
         <input type="text" name="enzyType" placeholder="Type/Dosage" id="enzyType">
-        <input type="submit" value="Create">
+        <label></label>
+        <input type="submit" value="Update">
     </form>
     <?php if ($msg): ?>
     <p><?=$msg?></p>
@@ -96,3 +98,4 @@ if (isset($_GET['medID'])) {
 </div>
 
 <?=template_footer()?>
+
